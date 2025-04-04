6c2e3a769a74059c33d4abff0b042e4086052eb4
Ricardo Bermúdez Bermúdez <rbermudez@MacBook-Air-de-Ricardo.local>
2025-04-03 19:31:02 -0600

    Fix code series 5

--- automation/delete_project.py
+++ automation/delete_project.py
@@ -4,9 +4,8 @@ from metaservlet.api import delete_project
 def main(projects: list):
   for project in projects:
     print('-----', project)
-    project_name = project[0]
     try:
-      delete_project(project_name)
+      delete_project(project[0])
     except Exception as e:
       print('ERROR: ', e.args)
       continue
