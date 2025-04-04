06d084e3c90422ce3278e60f0d0d160872b33f62
Ricardo Bermúdez Bermúdez <rbermudez@MacBook-Air-de-Ricardo.local>
2025-04-04 13:09:31 -0600

    Fix create_authorization command

--- metaservlet/api.py
+++ metaservlet/api.py
@@ -124,12 +124,18 @@ def delete_authorization(
 
 def create_authorization(
   project_name: str,
-  group_name: str,
+  user_group_name: str,
   authorization_type: str,
   authorization_entity: str='Group',
 ) -> dict:
+  if not project_name:
+    raise Exception('Project name can not be empty.')
+  if not user_group_name:
+    raise Exception('User group name can not be empty.')
+  if not authorization_type in ['ReadOnly', 'ReadWrite']:
+    raise Exception('<storage> invalid value.')
   request_params = {
-    'groupLabel': group_name,
+    'groupLabel': user_group_name,
     'projectName': project_name,
     'authorizationEntity': authorization_entity,
     'authorizationType': authorization_type,
