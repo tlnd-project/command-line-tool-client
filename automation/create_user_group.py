bcf943f3188f84026d8c984dbadbd3298b05d084
Ricardo Bermúdez Bermúdez <rbermudez@MacBook-Air-de-Ricardo.local>
2025-04-03 20:46:50 -0600

    Fix code series 6

--- automation/create_user_group.py
+++ automation/create_user_group.py
@@ -7,6 +7,8 @@ def main(user_groups: list):
     try:
       if not user_group_exist(user_group_name):
         create_user_group(user_group_name, description)
+      else:
+        raise Exception(f'A user group called "{user_group_name}" already exist')
     except Exception as e:
       print('ERROR: ', e.args)
       continue
