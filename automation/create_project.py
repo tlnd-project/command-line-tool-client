8684dcfc578da8c5bb28846ef7ce1a6eb2695183
Ricardo Bermúdez Bermúdez <rbermudez@MacBook-Air-de-Ricardo.local>
2025-04-03 20:24:49 -0600

    Fix code series 5

--- automation/create_project.py
+++ automation/create_project.py
@@ -6,9 +6,12 @@ def main(projects: list):
     print('-----', project)
     project_name, storage, git_location, git_login, git_pass = project
     try:
-      create_project(
-        project_name, storage, git_location, git_login, git_pass
-      )
+      if not project_exists(project_name):
+        create_project(
+          project_name, storage, git_location, git_login, git_pass
+        )
+      else:
+        raise Exception(f'{project_name} already exist')
     except Exception as e:
       print('ERROR: ', e.args)
       continue
--- automation/delete_project.py
+++ automation/delete_project.py
@@ -1,10 +1,12 @@
-from metaservlet.api import delete_project
+from metaservlet.api import delete_project, project_exists
 
 
 def main(projects: list):
   for project in projects:
     print('-----', project)
     try:
+      if not project_exists(project[0]):
+        raise Exception(f'A project called {project[0]} does not exist')
       delete_project(project[0])
     except Exception as e:
       print('ERROR: ', e.args)
