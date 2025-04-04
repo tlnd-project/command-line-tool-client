4c785690c7790a3ccf7f42dcc2e5afc20e36ccf0
Ricardo Bermúdez Bermúdez <rbermudez@MacBook-Air-de-Ricardo.local>
2025-04-03 20:19:01 -0600

    Fix code series 5

--- automation/create_project.py
+++ automation/create_project.py
@@ -6,12 +6,9 @@ def main(projects: list):
     print('-----', project)
     project_name, storage, git_location, git_login, git_pass = project
     try:
-      if not project_exists(project_name):
-        create_project(
-          project_name, storage, git_location, git_login, git_pass
-        )
-      else:
-        raise Exception(f'{project_name} already exist')
+      create_project(
+        project_name, storage, git_location, git_login, git_pass
+      )
     except Exception as e:
       print('ERROR: ', e.args)
       continue
