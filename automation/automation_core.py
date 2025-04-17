4930441d3ee7b9d55ede6b8e7d9b512fb86d0910
Ricardo Bermúdez Bermúdez <rbermudez@MacBook-Air-de-Ricardo.local>
2025-04-16 18:10:12 -0600

    Fix host restrictions

--- automation/automation_core.py
+++ automation/automation_core.py
@@ -5,12 +5,11 @@ from settings.credentials import CURRENT_HOST_NAME
 def run_command(command: Callable, items: list):
   for item in items:
     *item_content, item_host_restriction = item
-    item_host_restriction = item_host_restriction.split(';')
-    if (
-      len(item_host_restriction) > 0 
-      and not CURRENT_HOST_NAME in item_host_restriction
-    ):
-      continue
+
+    if not item_host_restriction == "":
+      item_host_restriction = item_host_restriction.split(';')
+      if not CURRENT_HOST_NAME in item_host_restriction:
+        continue
 
     print('######', item)
     try:
