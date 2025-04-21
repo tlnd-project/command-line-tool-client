16fb9cfd5dbb3dc7bb0abf8ce4c2b030adb33297
Ricardo Bermúdez Bermúdez <rbermudez@MacBook-Air-de-Ricardo.local>
2025-04-21 14:00:47 -0600

    Command(create_project): Add password encryption to git password

--- automation/command/create_project.py
+++ automation/command/create_project.py
@@ -1,4 +1,10 @@
 from metaservlet.api import project_exists, create_project
+from utilities.bitbucket_files_management import download_file
+from utilities.encryption import decrypt
+from settings.credentials import JAR_DECRYPTION_PATH
+
+
+KEY_NAME = 'dtcc.key'
 
 
 def process_item(project: list):
@@ -6,6 +12,10 @@ def process_item(project: list):
 
   if project_exists(project_name):
     raise Exception(f'{project_name} already exist')
+  
+  key_path = download_file(KEY_NAME)
+  git_pass = decrypt(git_pass, key_path, JAR_DECRYPTION_PATH)
+  
   create_project(
     project_name, storage, git_location, git_login, git_pass
   )
