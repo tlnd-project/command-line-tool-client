8e0c28b7b7539e86f3a5feee87cd30ebe983c37e
Fernando Rodriguez <ferthus@live.com>
2025-05-30 10:55:32 -0600

    feat: add key password_tac_encrypt

--- utilities/remote_conexion.py
+++ utilities/remote_conexion.py
@@ -1,9 +1,9 @@
 from fabric import Connection, task
 
 SERVERS = [
-    {"host": "192.168.1.10", "user": "usuario1", "password": "clave1", "port": 22},
-    {"host": "192.168.1.11", "user": "usuario2", "password": "clave2", "port": 2222},
-    {"host": "192.168.1.12", "user": "usuario3", "password": "clave3", "port": 22},
+    {"host": "192.168.1.10", "user": "usuario1", "password": "clave1", "port": 22, "password_tac_encrypt": "f3434g545t4t45t23"},
+    {"host": "192.168.1.11", "user": "usuario2", "password": "clave2", "port": 2222, "password_tac_encrypt": "f34f34f34dvbvobocvb"},
+    {"host": "192.168.1.12", "user": "usuario3", "password": "clave3", "port": 22, "password_tac_encrypt": "f3434g09908ddfgregr3"},
 ]
 for server in SERVERS:
     conn = Connection(
@@ -13,4 +13,4 @@ for server in SERVERS:
         connect_kwargs={"password": server["password"]}
     )
     # set environ password
-    conn.run('TAC_PASSWORD=<password_encrypt>')
+    conn.run(f'TAC_PASSWORD={server["password_tac_encrypt"]}')
