98305b6fda1cb75feb320bc4aae7b4d06d8e55b4
Fernando Rodriguez <ferthus@live.com>
2025-08-07 11:55:51 -0600

    fix: complete test validate csv files

--- test/self-test.py
+++ test/self-test.py
@@ -3,6 +3,8 @@ from test_bitbucket import test_download_file
 from test_scrapper import test_login_logout
 from test_metaservlet import test_get_info_user
 from test_decrypt import test_decrypt_bitbucket, test_decrypt_talend_password
+from test_files_csv import validate_all_txt_from_project
+
 
 logger = logging.getLogger(__name__)
 
@@ -25,3 +27,5 @@ logger.info(f"[=== > TEST METASERVLET < === ]")
 test_get_info_user()
 
 # retrieve all files CSV and validate the format.
+logger.info(f"[=== > TEST FILES TXT < === ]")
+validate_all_txt_from_project()
